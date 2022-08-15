d65be972a6472ee8da91810b5bd88a46cffbb8f1
Artur <torbogoshev@yandex.ru>
2022-08-15 22:10:20 +0300

    merge dev branch

---
+++ resources/views/tournaments/all.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="btn btn-outline-primary float-end" id="tournamentCreate">
+        Создать турнир
+    </div>
+
+    <table class="table">
+        <tr>
+            <th>Название</th>
+            <th>Время начала</th>
+            <th>Создал</th>
+        </tr>
+        @foreach($tournaments as $tournament)
+            <tr>
+                <td>{{ $tournament->name }}</td>
+                <td>{{ $tournament->tournament_start }}</td>
+{{--                <td>{{ $tournament->creator->login }}</td>--}}
+            </tr>
+        @endforeach
+    </table>
+@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js(['resources/js/app.js'], 'public/js')
-    .sourceMaps();
+    .sass('resources/css/app.scss', 'public/css')
+    .sourceMaps()
+;
