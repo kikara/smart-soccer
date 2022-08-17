772f986842ccea46ec9ff66860154876f43c8875
Artur Torbogoshev <torbogoshev@yandex.ru>
2022-08-17 21:40:34 +0300

    fix telegram request :zip:

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,5 +8,8 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-
+    public function index()
+    {
+        return view('main');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('main');
-});
+Route::get('/', 'IndexController@index');
 
 Auth::routes();
 //Route::get('/goalTest', function () {
