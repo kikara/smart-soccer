8026cf51d909220cb767841a9f5491839f79c14e
Torbogoshev Artur <torbogoshev@yandex.ru>
2022-08-17 21:53:45 +0300

    test commit :+1:

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -14,6 +14,7 @@
         <div class="px-4 py-5 my-5 text-center">
             <div class="col-lg-6 mx-auto">
                 <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <h1>hello world</h1>
                 </div>
             </div>
         </div>
