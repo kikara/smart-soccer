b838876612287423761cc38931ad51173b40227f
Artur Torbogoshev <torbogoshev@yandex.ru>
2022-12-07 22:26:19 +0300

    debug page fix

--- resources/views/debug/index.blade.php
+++ resources/views/debug/index.blade.php
@@ -2,18 +2,19 @@
 
 @section('content')
     <script>
-        document.addEventListener('DOMContentLoaded', (e) => {
-            let debug = new Debug();
-            debug.init();
-        });
+        document.addEventListener('DOMContentLoaded', e => (new Debug()).init());
     </script>
-    <h1>Отладочная страница</h1>
-    <div class="js-container">
-        <button class="btn btn-secondary js-connect">Попытка Соединения</button>
-        <button class="btn btn-primary js-prepare-game">Занять стол</button>
-        <button class="btn btn-primary js-start">Старт</button>
-        <button class="btn btn-primary js-blue">Синий</button>
-        <button class="btn btn-primary js-red">Красный</button>
-        <button class="btn btn-primary js-test">9 голов за Синего</button>
+    <style>
+        div>button {width: 200px}
+    </style>
+    <div class="js-container container">
+        <div class="d-flex flex-column gap-2 flex-wrap align-items-center">
+            <button class="btn btn-secondary js-connect">Попытка Соединения</button>
+            <button class="btn btn-primary js-test">9 голов за Синего</button>
+            <button class="btn btn-primary js-prepare-game">Занять стол</button>
+            <button class="btn btn-primary js-start">Старт</button>
+            <button class="btn btn-primary js-blue">Синий</button>
+            <button class="btn btn-primary js-red">Красный</button>
+        </div>
     </div>
 @endsection
