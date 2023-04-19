df4c9c54855f811c7ffa30e2c25486a6ef3490ff
Artur Torbogoshev <torbogoshev@yandex.ru>
2023-04-19 22:35:37 +0300

    app - profile

---
+++ resources/js/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+export default {
+    name: "App",
+    props: ['isAuth'],
+    created() {
+        if (this.isAuth) {
+            axios.get('/users')
+                .then((response) => {
+                    this.$store.commit('setAuthenticated');
+                    this.$store.commit('setAuthenticatedUser', response.data.data);
+                });
+        }
+    },
+    mounted() {
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
