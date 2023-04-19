026eddcc0027a0feab3d8c1338acf29e9449e420
Artur Torbogoshev <torbogoshev@yandex.ru>
2023-04-19 22:37:48 +0300

    user module

---
+++ resources/js/store/userModule.js
@@ -0,0 +1,18 @@
+export const userModule = {
+    state: () => ({
+        is_auth: false,
+        user: {
+            id: 0,
+            avatar_path: 0,
+            login: ''
+        }
+    }),
+    mutations: {
+        setAuthenticatedUser(state, user) {
+            state.user = user;
+        },
+        setAuthenticated(state) {
+            state.is_auth = true;
+        },
+    }
+};
