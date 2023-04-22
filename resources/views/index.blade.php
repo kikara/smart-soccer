d9a65d2fbf9c060f4994f31191fc82d9cad0640c
Artur Torbogoshev <torbogoshev@yandex.ru>
2023-04-22 15:28:05 +0300

    index app view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app')
 
 @section('head')
-    <script src="{{ asset('js/vuetify.js') }}"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik+Wet+Paint&family=Teko:wght@700&display=swap" rel="stylesheet">
@@ -11,6 +8,6 @@
 
 @section('content')
     <div id="app">
-        <game-index-component is-auth="{{ auth()->check() }}"></game-index-component>
+        <app is-auth="{{ auth()->check() }}" user-id="{{ auth()->check() ? auth()->user()->id : 0 }}"></app>
     </div>
 @endsection
