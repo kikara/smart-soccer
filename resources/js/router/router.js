94dc190010cf8f02e0b6d888daab9e0779a4580f
Artur Torbogoshev <torbogoshev@yandex.ru>
2023-04-22 15:22:36 +0300

    router

--- resources/js/router/router.js
+++ resources/js/router/router.js
@@ -1,6 +1,10 @@
 import GameIndexComponent from "../components/game/GameIndexComponent.vue";
 import ProfileComponent from "../components/profile/ProfileComponent.vue";
 import {createRouter, createWebHistory} from "vue-router";
+import Statistic from "../components/pages/Statistics.vue";
+import Setting from "../components/pages/Settings.vue";
+import Sounds from "../components/pages/Sounds.vue";
+import EventSounds from "../components/pages/EventSounds.vue";
 
 
 const routes = [
@@ -11,6 +15,22 @@ const routes = [
     {
         path: '/info/personal/:id',
         component: ProfileComponent
+    },
+    {
+        path: '/info/personal/stats',
+        component: Statistic
+    },
+    {
+        path: '/info/personal/settings',
+        component: Setting
+    },
+    {
+        path: '/info/sounds/without',
+        component: Sounds
+    },
+    {
+        path: '/info/sounds/with',
+        component: EventSounds,
     }
 ];
 
