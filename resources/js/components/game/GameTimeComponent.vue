9eb1734e211725dd8fef64fb0ebec9e75c3a2803
Artur Torbogoshev <torbogoshev@yandex.ru>
2023-05-13 01:39:48 +0300

    :fire: game time component

--- resources/js/components/game/GameTimeComponent.vue
+++ resources/js/components/game/GameTimeComponent.vue
@@ -6,9 +6,56 @@
 </template>
 
 <script>
+import {addListener, getCurrentState} from "../../game/events";
+
 export default {
     name: "GameTimeComponent",
-    props: ['time']
+    data() {
+        return {
+            seconds: 0,
+            time: '00:00',
+            startTime: 0,
+            interval: null,
+        }
+    },
+    mounted() {
+        const state = getCurrentState();
+
+        if (state.game_started) {
+            this.init(state);
+        } else {
+            addListener(this.eventHandle);
+        }
+    },
+    methods: {
+        init(state) {
+            this.startTime = state.start_time;
+            this.startTimer();
+        },
+        startTimer() {
+            const now = Math.floor(new Date().valueOf() / 1000);
+            this.seconds = now - this.startTime;
+            this.interval = setInterval(this.timer, 1000);
+        },
+        timer() {
+            this.seconds++;
+
+            const minutes = Math.floor(this.seconds / 60);
+            const seconds = this.seconds - (minutes * 60);
+
+            this.time = (minutes > 9 ? minutes : '0' + minutes) + ':' + (seconds > 9 ? seconds : '0' + seconds);
+        },
+        stop() {
+            clearInterval(this.interval);
+        },
+        eventHandle(event, state) {
+            switch (event) {
+                case 'start':
+                    this.init(state);
+                    break;
+            }
+        },
+    }
 }
 </script>
 
