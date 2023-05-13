2b76d21969a8497be1ae5f0619f13f399372e48d
Artur Torbogoshev <torbogoshev@yandex.ru>
2023-05-13 17:42:44 +0300

    game mode component

---
+++ resources/js/components/game/GameModeComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <versus-component
+    ></versus-component>
+
+    <fight-component>
+    </fight-component>
+
+</template>
+
+<script>
+import VersusComponent from "./VersusComponent.vue";
+import FightComponent from "./FightComponent.vue";
+
+export default {
+    name: "GameModeComponent",
+    components: {
+        FightComponent,
+        VersusComponent
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
