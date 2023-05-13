835f51138edde2d5370c543f1b453893ddb252bb
Artur Torbogoshev <torbogoshev@yandex.ru>
2023-05-13 17:42:56 +0300

    home component

---
+++ resources/js/components/game/HomeComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <navigation-component
+    ></navigation-component>
+
+    <last-games-component
+    ></last-games-component>
+</template>
+
+<script>
+import NavigationComponent from "./NavigationComponent.vue";
+import LastGamesComponent from "./LastGamesComponent.vue";
+
+export default {
+    name: "HomeComponent",
+    components: {LastGamesComponent, NavigationComponent}
+}
+</script>
+
+<style scoped>
+
+</style>
